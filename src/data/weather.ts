77cd2fe1ba66294f276b23e1f62186652109d536
lirtual <lirtual@qq.com>
2024-05-13 09:54:08 +0800

    feat: Add Weather Text Chinese

--- src/data/weather.ts
+++ src/data/weather.ts
@@ -121,23 +121,23 @@ export class Weather {
 export function getWeatherStartMessage(weatherType: WeatherType): string {
   switch (weatherType) {
     case WeatherType.SUNNY:
-      return 'The sunlight got bright!';
+      return '阳光变得耀眼！';
     case WeatherType.RAIN:
-      return 'A downpour started!';
+      return '下起了雨！';
     case WeatherType.SANDSTORM:
-      return 'A sandstorm brewed!';
+      return '刮起了沙尘暴！';
     case WeatherType.HAIL:
-      return 'It started to hail!';
+      return '下起了冰雹！';
     case WeatherType.SNOW:
-      return 'It started to snow!';
+      return '下起了雪！';
     case WeatherType.FOG:
-      return 'A thick fog emerged!'
+      return '出现了浓雾！';
     case WeatherType.HEAVY_RAIN:
-      return 'A heavy downpour started!'
+      return '下起了倾盆大雨！';
     case WeatherType.HARSH_SUN:
-      return 'The sunlight got hot!'
+      return '阳光变得灼热！';
     case WeatherType.STRONG_WINDS:
-      return 'A heavy wind began!';
+      return '刮起了强风！';
   }
 
   return null;
@@ -146,23 +146,23 @@ export function getWeatherStartMessage(weatherType: WeatherType): string {
 export function getWeatherLapseMessage(weatherType: WeatherType): string {
   switch (weatherType) {
     case WeatherType.SUNNY:
-      return 'The sunlight is strong.';
+      return '阳光强烈！';
     case WeatherType.RAIN:
-      return 'The downpour continues.';
+      return '大雨持续！';
     case WeatherType.SANDSTORM:
-      return 'The sandstorm rages.';
+      return '沙暴肆虐！';
     case WeatherType.HAIL:
-      return 'Hail continues to fall.';
+      return '冰雹持续落下！';
     case WeatherType.SNOW:
-      return 'The snow is falling down.';
+      return '雪花飘落！';
     case WeatherType.FOG:
-      return 'The fog continues.';
+      return '浓雾弥漫！';
     case WeatherType.HEAVY_RAIN:
-      return 'The heavy downpour continues.'
+      return '暴雨倾盆！';
     case WeatherType.HARSH_SUN:
-      return 'The sun is scorching hot.'
+      return '烈日炎炎！';
     case WeatherType.STRONG_WINDS:
-      return 'The wind blows intensely.';
+      return '狂风呼啸！';
   }
 
   return null;
@@ -171,9 +171,9 @@ export function getWeatherLapseMessage(weatherType: WeatherType): string {
 export function getWeatherDamageMessage(weatherType: WeatherType, pokemon: Pokemon): string {
   switch (weatherType) {
     case WeatherType.SANDSTORM:
-      return getPokemonMessage(pokemon, ' is buffeted\nby the sandstorm!');
+      return getPokemonMessage(pokemon, '被沙暴\n吹得睁不开眼!');
     case WeatherType.HAIL:
-      return getPokemonMessage(pokemon, ' is pelted\nby the hail!');
+      return getPokemonMessage(pokemon, '被冰雹\n砸得生疼!');
   }
 
   return null;
@@ -182,23 +182,23 @@ export function getWeatherDamageMessage(weatherType: WeatherType, pokemon: Pokem
 export function getWeatherClearMessage(weatherType: WeatherType): string {
   switch (weatherType) {
     case WeatherType.SUNNY:
-      return 'The sunlight faded.';
+      return '阳光消散了。';
     case WeatherType.RAIN:
-      return 'The rain stopped.';
+      return '雨停了。';
     case WeatherType.SANDSTORM:
-      return 'The sandstorm subsided.';
+      return '沙暴平息了。';
     case WeatherType.HAIL:
-      return 'The hail stopped.';
+      return '冰雹停了。';
     case WeatherType.SNOW:
-      return 'The snow stopped.';
+      return '雪停了。';
     case WeatherType.FOG:
-      return 'The fog disappeared.'
+      return '雾散了。';
     case WeatherType.HEAVY_RAIN:
-      return 'The heavy rain stopped.'
+      return '暴雨停了。';
     case WeatherType.HARSH_SUN:
-      return 'The harsh sunlight faded.'
+      return '烈日消退了。';
     case WeatherType.STRONG_WINDS:
-      return 'The heavy wind stopped.';
+      return '狂风止息了。';
   }
 
   return null;
@@ -207,33 +207,35 @@ export function getWeatherClearMessage(weatherType: WeatherType): string {
 export function getTerrainStartMessage(terrainType: TerrainType): string {
   switch (terrainType) {
     case TerrainType.MISTY:
-      return 'Mist swirled around the battlefield!';
+      return '迷雾笼罩了整个战场！';
     case TerrainType.ELECTRIC:
-      return 'An electric current ran across the battlefield!';
+      return '电流窜过战场！';
     case TerrainType.GRASSY:
-      return 'Grass grew to cover the battlefield!';
+      return '青草覆盖了整个战场！';
     case TerrainType.PSYCHIC:
-      return 'The battlefield got weird!';
+      return '战场变得奇异起来！';
   }
 }
 
 export function getTerrainClearMessage(terrainType: TerrainType): string {
   switch (terrainType) {
     case TerrainType.MISTY:
-      return 'The mist disappeared from the battlefield.';
+      return '战场上的迷雾消散了。';
     case TerrainType.ELECTRIC:
-      return 'The electricity disappeared from the battlefield.';
+      return '战场上的电流消失了。';
     case TerrainType.GRASSY:
-      return 'The grass disappeared from the battlefield.';
+      return '战场上的青草消失了。';
     case TerrainType.PSYCHIC:
-      return 'The weirdness disappeared from the battlefield!';
+      return '战场上的奇异现象消失了！';
   }
 }
 
 export function getTerrainBlockMessage(pokemon: Pokemon, terrainType: TerrainType): string {
-  if (terrainType === TerrainType.MISTY)
-    return getPokemonMessage(pokemon, ` surrounds itself with a protective mist!`);
-  return getPokemonMessage(pokemon, ` is protected by the ${Utils.toReadableString(TerrainType[terrainType])} Terrain!`);
+  if (terrainType === TerrainType.MISTY) {
+    return getPokemonMessage(pokemon, ' 身周环绕起保护性的迷雾！');
+  } else {
+    return getPokemonMessage(pokemon, ` 受${Utils.toReadableString(TerrainType[terrainType])}地形的保护！`);
+  }  
 }
 
 interface WeatherPoolEntry {
